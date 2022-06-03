8b90947a5ad98a08beb9e39da9970b4b40507a57
hopecris31 <hope.crisafi@gmail.com>
2022-06-02 22:23:28 -0400

    method for creating poker hand in unit testing

--- src/proj4/PokerHandTesting.java
+++ src/proj4/PokerHandTesting.java
@@ -33,10 +33,36 @@ public class PokerHandTesting {
     }
 
     public static void testPokerHand(Testing testSuite) {
-        String[] flush1 = {"2", "Spades", "3", "Spades", "5", "Spades", "7", "Spades", "9", "Spades"};
-        PokerHand flush = makeHandFromArray(flush1);
-        System.out.println(flush);
+        String[] flush1List = {"2", "Spades", "3", "Spades", "5", "Spades", "7", "Spades", "9", "Spades"};
+        PokerHand flush1 = makeHandFromArray(flush1List);
+        String[] flush2List = {"8", "Hearts", "9", "Hearts", "10", "Hearts", "11", "Hearts", "12", "Hearts"};
+        PokerHand flush2 = makeHandFromArray(flush2List);
+        String[] twoPair1List = {"8", "Hearts", "9", "Spades", "8", "Diamonds", "9", "Hearts", "12", "Hearts"};
+        PokerHand twoPair1 = makeHandFromArray(twoPair1List);
+        String[] twoPair2List = {"2", "Hearts", "3", "Spades", "2", "Diamonds", "3", "Hearts", "5", "Hearts"};
+        PokerHand twoPair2 = makeHandFromArray(twoPair2List);
+        String[] twoPair3List = {"2", "Hearts", "3", "Spades", "2", "Diamonds", "3", "Hearts", "6", "Hearts"};
+        PokerHand twoPair3 = makeHandFromArray(twoPair3List);
+        String[] pair1List = {"5", "Hearts", "5", "Spades", "2", "Diamonds", "3", "Hearts", "6", "Hearts"};
+        PokerHand pair1 = makeHandFromArray(pair1List);
+        String[] pair2List = {"7", "Hearts", "7", "Spades", "2", "Diamonds", "3", "Hearts", "6", "Hearts"};
+        PokerHand pair2 = makeHandFromArray(pair2List);
+        String[] pair3List = {"7", "Hearts", "7", "Spades", "2", "Diamonds", "3", "Hearts", "14", "Hearts"};
+        PokerHand pair3 = makeHandFromArray(pair3List);
+        String[] highCard1List = {"2", "Hearts", "3", "Spades", "4", "Diamonds", "5", "Hearts", "6", "Hearts"};
+        PokerHand highCard1 = makeHandFromArray(highCard1List);
+        String[] highCard2List = {"2", "Hearts", "3", "Spades", "4", "Diamonds", "5", "Hearts", "14", "Hearts"};
+        PokerHand highCard2 = makeHandFromArray(highCard2List);
 
 
+        Testing.assertEquals("testing flush to flush, one with higher high card", -1, flush1.compareTo(flush2));
+        Testing.assertEquals("testing flush to flush, both same exact ranks", 0, flush1.compareTo(flush1));
+        Testing.assertEquals("testing two pair to two pair, both same exact ranks", 0, twoPair1.compareTo(twoPair1));
+        Testing.assertEquals("testing two pair to two pair, one with lower ranks", 1, twoPair1.compareTo(twoPair2));
+        Testing.assertEquals("testing two pair to two pair, one with higher high card", -1, twoPair2.compareTo(twoPair3));
+        Testing.assertEquals("testing two pairs, one with higher ranks", -1, pair1.compareTo(pair2));
+        Testing.assertEquals("testing two pairs, one with higher high card", -1, pair1.compareTo(pair3));
+        Testing.assertEquals("testing two pairs, all exact same ranks", 0, pair1.compareTo(pair1));
+        Testing.assertEquals("testing high card", -1, highCard1.compareTo(highCard2));
     }
 }
